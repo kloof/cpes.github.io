62eab5303e3ee7e4abf5a4ab38fac0a380f65201
asmaa <projectcodekw@gmail.com>
2024-03-07 01:16:38 +0300

    added sponsors

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,9 @@ const bodyMsg = document.getElementById('body-msg');
 const noBTN = document.getElementById('btn2');
 const yesBTN = document.getElementById('btn1');
 const archive_23 = document.getElementById('2023-folder');
+const sponsors24 = document.getElementById('sponsors-folder');
 const exit_archive_23 = document.getElementById('exit-2023-archive');
-
+const exit_sponsors = document.getElementById('exit-sponsors');
 
 // Arrays of values for the body msg 
 const listC = ["68 65 6C 6C 6F 3F", "is Talal the 100th president of CPES?", "Do u know Project Code Kw?", "HlElO>>?"];
@@ -24,38 +25,66 @@ function updateContent() {
     bodyMsg.textContent = getRandomValue(listC);
 }
 
-// Function to hide the message container
-function hideMessage() {
-    draggableDiv.style.display = 'none';
+function noBtnClicked() {
+    if (bodyMsg.textContent === "Do u know Project Code Kw?") {
+
+        window.location.href = 'https://home.projectcodekw.com/';
+        draggableDiv.style.display = 'none';
+    }
+    else {
+        draggableDiv.style.display = 'none';
+    }
+}
+
+function yesBtnClicked() {
+    const pc_logo = document.getElementById('pc-logo');
+
+    if (bodyMsg.textContent === "Do u know Project Code Kw?") {
+        document.body.style.backgroundColor = "#8bd8bd";
+        draggableDiv.style.display = 'none';
+    }
+    else {
+        draggableDiv.style.display = 'none';
+    }
 }
 
 // Function to show the message container
 function showMessage() {
+
     draggableDiv.style.display = 'block';
     updateContent();
 }
 
 
+function hideMessage() {
+    draggableDiv.style.display = 'none';
+}
 
-// Add a click event listener to the "X" button
 headerX.addEventListener('click', hideMessage);
-noBTN.addEventListener('click', hideMessage);
-yesBTN.addEventListener('click', hideMessage);
+noBTN.addEventListener('click', noBtnClicked);
+yesBTN.addEventListener('click', yesBtnClicked);
 exit_archive_23.addEventListener('click', hideArchive2023);
+
+//FOR SPONSORS
+exit_sponsors.addEventListener('click', hideSponsors24);
 
 
 // Add a click event listener to the toggle link
 toggleLink.addEventListener('click', showMessage);
-toggleLink.addEventListener('click', showMessage);
 archive_23.addEventListener('click', showArchive2023);
 
+//SPONSORS
+sponsors24.addEventListener('click', showSponsors24);
+
 //NOTE PAD STUFF
-// Get references to the elements
 const notepadToggle2 = document.getElementById('notepad-toggle2');
 const notepadToggle1 = document.getElementById('notepad-toggle');
 const notepadContainer = document.getElementById('notepad-container');
 const exitX = document.getElementById('exit-note');
 const archiveContainer = document.getElementById('archive-container');
+
+const sponsorsContainer = document.getElementById('sponsors-container');
+
 
 // Function to hide the message container
 function hideNote() {
@@ -68,13 +97,21 @@ function showNote() {
     updateContent();
 }
 
-function showArchive2023()
-{
+function showArchive2023() {
     archiveContainer.style.display = 'block';
+}
+
+function showSponsors24() {
+    sponsorsContainer.style.display = 'block';
 }
 
 function hideArchive2023() {
     archiveContainer.style.display = 'none';
+}
+
+
+function hideSponsors24() {
+    sponsorsContainer.style.display = 'none';
 }
 
 // Add a click event listener to the "X" button
@@ -85,12 +122,11 @@ exitX.addEventListener('click', hideNote);
 notepadToggle1.addEventListener('click', showNote);
 notepadToggle2.addEventListener('click', showNote);
 
-
 // NOTEPAD COLOR
 // Get references to the elements
 const notepadInput = document.getElementById('notepad-input');
-const cpesWhite = document.getElementById('cpes-white'); 
-const cpesBlue = document.getElementById('cpes-blue'); 
+const cpesWhite = document.getElementById('cpes-white');
+const cpesBlue = document.getElementById('cpes-blue');
 
 // Get references to the elements of the captions
 const columnHeaders = document.querySelectorAll('.column header');
@@ -110,8 +146,7 @@ function changeBackgroundColorAndImage(color) {
         cpesBlue.style.display = 'none';
     }
 
-    if (color.toLowerCase()==='black')
-    {
+    if (color.toLowerCase() === 'black') {
         columnHeaders.forEach(header => {
             header.style.color = 'white';
         });
